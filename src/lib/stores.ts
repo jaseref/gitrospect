64abdd30c833506c0a9497f355b7a914207ab4ff
mattref <137362246+mattref@users.noreply.github.com>
2024-03-10 22:47:34 -0400

    Early implementation of Select Repo landing page

---
+++ src/lib/stores.ts
@@ -0,0 +1,3 @@
+import { writable } from 'svelte/store';
+
+export const selectedRepo = writable<string>();
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { Button } from '$lib/components/ui/button';
+    import { open } from '@tauri-apps/api/dialog';
+    import { selectedRepo } from '$lib/stores';
+    // import { goto } from '$app/navigation';
+
+    async function onClick() {
+        const folderPath = await open({
+            title: 'Select a repository',
+            directory: true,
+            multiple: false
+        });
+
+        if (typeof folderPath === 'string') {
+            console.log('Setting path: ', folderPath);
+            selectedRepo.set(folderPath);
+            // TODO: Redirect user
+            // goto("/repo");
+        }
+    }
+</script>
+
+<div class="flex min-h-screen flex-col items-center justify-center">
+    <h1 class="absolute top-0 mt-14">Gitrospect</h1>
+    <div class="mt-24 flex flex-col gap-5">
+        <p>Open a repository</p>
+        <Button on:click={onClick}>Open</Button>
+    </div>
+</div>
